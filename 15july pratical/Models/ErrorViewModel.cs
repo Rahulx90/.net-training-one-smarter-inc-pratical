eec06185af220ca926304c12846b0b46f8273826
Rahul Paghrut <rahulpaghrut03@gmail.com>
2026-07-26 15:46:34 +0530

    Delete 15july pratical directory

--- 15july pratical/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _15july_pratical.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
