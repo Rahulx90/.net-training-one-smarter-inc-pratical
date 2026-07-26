a64aad6e91e91d43f4fe2ad587f3d2357230b75b
rahulx90 <rahulpaghrut03@gmail.com>
2026-07-26 15:54:23 +0530

    Merge master into main

--- Desktop/.netfunction/Assignment/Assignment2-8july/obj/Debug/net10.0/Assignment2-8july.AssemblyInfo.cs
+++ Desktop/.netfunction/Assignment/Assignment2-8july/obj/Debug/net10.0/Assignment2-8july.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment2-8july")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f35a3a9bbf9d10662c4f4483ee79eaac1c7b9002")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment2-8july")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment2-8july")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Desktop/.netfunction/Pratical/11julypratical/obj/Debug/net10.0/11julypratical.AssemblyInfo.cs
+++ Desktop/.netfunction/Pratical/11julypratical/obj/Debug/net10.0/11julypratical.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("11julypratical")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f35a3a9bbf9d10662c4f4483ee79eaac1c7b9002")]
 [assembly: System.Reflection.AssemblyProductAttribute("11julypratical")]
 [assembly: System.Reflection.AssemblyTitleAttribute("11julypratical")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
